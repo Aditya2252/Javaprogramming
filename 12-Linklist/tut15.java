431082e6ea47e7a77517f7f94e96092bc6dbb36b
Aditya2252 <113898549+Aditya2252@users.noreply.github.com>
2023-08-24 23:29:34 +0530

    java

--- 12-Linklist/tut15.java
+++ 12-Linklist/tut15.java
@@ -8,22 +8,63 @@ public class tut15 {
 
     }
   }
-  public static Node head;
-  public static Node tail;
+  public static Node head = null;
+  public static Node tail=null;
 
   public void insert(int data){
 
     Node newNode=new Node(data);
+    // if(head==null){
+        // head=tail=newNode;
+    // }
+    // newNode.next=;
+    // head=newNode;
+
     if(head==null){
-        head=tail=newNode;
+      head = tail=newNode;
+      // newNode.next = null;
+      // return;
+    }
+
+    Node temp = head;
+
+    while(temp.next != null){
+      temp = temp.next;
+    }
+
+    temp.next = newNode;
+    newNode.next = null;
+    tail=newNode;
+    
+   }
+
+   public void insertlast(int data){
+    Node newNode=new Node(data);
+    if(head==null){
+      head=tail=newNode;
+    }
+    tail.next=newNode;
+    tail=newNode;
+
+   }
+
+   public void insertfirst(int data){
+    Node newNode=new Node (data);
+    if(head==null){
+      head=tail=newNode;
     }
     newNode.next=head;
     head=newNode;
+   }
 
-  }
 
   public void print(){
-    
+    Node temp=head;
+    while(temp!=null){
+      System.out.println(temp.data);
+      temp=temp.next;
+
+    }
   }
 
 
@@ -33,6 +74,10 @@ public class tut15 {
         ll.insert(2);
         ll.insert(3);
         ll.insert(4);
+
+        ll.insertlast(5);
+        ll.insertfirst(0);
+        ll.print();
 
         
     }
